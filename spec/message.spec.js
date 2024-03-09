eeadf6d7bc8b935bcc9bdcb989d015b0140001e4
Your Name"kamalaGIT <bandhavi.kk@gmail.com>
2024-03-09 09:03:10 -0600

    Completed

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -6,4 +6,19 @@ const Command = require('../command.js');
 
 describe("Message class", function() {
 
+    it("throws error if a name is NOT passed into the constructor as the first parameter", function() {
+        expect( function() { new Message();}).toThrow(new Error('Message Name is required.'));
+      });
+    
+      it("constructor sets name", function() {
+        let message = new Message('New message!');
+        expect(message.name).toEqual('New message!');
+      });
+    
+      it("contains a commands array passed into the constructor as 2nd argument", function() {
+        let commands = [new Command('STATUS_CHECK'), new Command('MOVE', 20)];
+        let message = new Message('New message!', commands);
+        expect(message.commands).toEqual(commands);
+
+});
 });
